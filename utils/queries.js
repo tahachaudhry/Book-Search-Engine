1ee00d1d09aa1963fe290b470894c326c59d3a4b
Taha Chaudhry <taha@nyu.edu>
2022-10-22 23:37:57 -0500

    final commit

---
+++ utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    {
+  me {
+    _id
+    username
+    email
+    password
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
+`
